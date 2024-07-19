3625c57fd0b614e2c1e5f54d2173a1d9baabae77
Esteban <Esteban-g-7@hotmail.com>
2024-07-19 03:40:50 -0500

    Add questions

---
+++ script.js
@@ -0,0 +1,52 @@
+const quiz = [
+  {
+      q:'Whos is the GOAT of football?',
+      options:['Lionel Messi','Cristiano Ronaldo','Neymar Jr','Eden Hazard'],
+      answer: 0
+  },
+  {
+      q:'What is the best football league in the world?',
+      options:['La Liga','Premier League','Ligue 1','Bundesliga'],
+      answer: 1
+  },
+  {
+      q:'Which country won the Euros 2024?',
+      options:['France','England','Spain','Portugal'],
+      answer: 2
+  },
+  {
+      q:'Who won the Copa America 2024?',
+      options:['Colombia','Uruguay','Canada','Argentina'],
+      answer: 3
+  },
+  {
+      q:'Who has the most Ballon d\'Or?',
+      options:['Lionel Messi','Lev Yashin','Johan Cruyff','Zinedine Zidane'],
+      answer: 0
+  },
+  {
+      q:'What football has the most Champions league trophies?',
+      options:['Barcelona','Real Madrid','Chelsea','Arsenal'],
+      answer: 1
+  },
+  {
+      q:'How many World Cups have there been?',
+      options:['54','17','22','31'],
+      answer: 2
+  },
+  {
+      q:'Who is the All time Premier League goal scorer?',
+      options:['Harry Kane','Sergio Aguero','Frank Lampard','Alan Shearer'],
+      answer: 3
+  },
+  {
+      q:'Who is the mst decorated player of all time?',
+      options:['Lionel Messi','Dani Alves','Andres Iniesta','David Alaba'],
+      answer: 0
+  },
+  {
+      q:'What month do the top european leagues start?',
+      options:['May','August','July','September'],
+      answer: 1
+  }
+]
